282f955d38d2ff34763090057a0632abae00b063
German D. Schneck <german.schneck@gmail.com>
2021-01-07 12:55:15 -0300

    initial proyect from local

---
+++ ligthbox.js
@@ -0,0 +1,36 @@
+
+
+
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesligth = document.querySelector('.agregar-imagen')
+const contenedorligth = document.querySelector('.imagen-ligth')
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+      aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+
+contenedorligth.addEventListener('click', (e)=>{
+  if(e.target !== imagenesligth){
+    contenedorligth.classList.toggle('show')
+    imagenesligth.classList.toggle('showimage')
+
+  }
+})
+
+
+
+
+
+
+
+const aparecerImagen = (imagen)=>{
+  imagenesligth.src = imagen
+  contenedorligth.classList.toggle('show')
+  imagenesligth.classList.toggle('showimage')
+
+
+
+}
